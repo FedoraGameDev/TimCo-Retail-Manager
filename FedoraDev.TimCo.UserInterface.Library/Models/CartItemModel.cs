d716830812c46adaa1d7adbb49cbc68d75120546
FedoraGameDev <fedora.game.dev@gmail.com>
2021-05-13 04:00:59 -0700

    Add to Cart now works ft SubTotal

---
+++ FedoraDev.TimCo.UserInterface.Library/Models/CartItemModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FedoraDev.TimCo.UserInterface.Library.Models
+{
+	public class CartItemModel
+	{
+		public ProductModel Product { get; set; }
+		public int QuantityInCart { get; set; }
+		public string DisplayText => $"{QuantityInCart}x {Product.ProductName}";
+	}
+}
--- FedoraDev.TimCo.UserInterface.WPF/ViewModels/SalesViewModel.cs
+++ FedoraDev.TimCo.UserInterface.WPF/ViewModels/SalesViewModel.cs
@@ -3,29 +3,33 @@ using FedoraDev.TimCo.UserInterface.Library.Api;
 using FedoraDev.TimCo.UserInterface.Library.Models;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace FedoraDev.TimCo.UserInterface.WPF.ViewModels
 {
 	public class SalesViewModel : Screen
 	{
+		#region Fields
 		private IProductEndpoint _productEndpoint;
-		private BindingList<ProductModel> _cart;
+		private BindingList<CartItemModel> _cart;
 		private BindingList<ProductModel> _products;
-		private int _itemQuantity;
+		private int _itemQuantity = 1;
+		private ProductModel _selectedProduct;
+		#endregion
 
-		public string SubTotal => "$0.00";
-		public string Tax => "$0.00";
-		public string Total => "$0.00";
-		public bool CanAddToCart => ItemQuantity > 0;
+		#region Properties
+		public string SubTotal => GetSubTotal().ToString("C");
+		public string Tax => GetTax().ToString("C");
+		public string Total => GetTotal().ToString("C");
+		public bool CanAddToCart => ItemQuantity > 0 && SelectedProduct?.QuantityInStock >= ItemQuantity;
 		public bool CanRemoveFromCart => false;
 		public bool CanCheckout => false;
 
-		public BindingList<ProductModel> Cart
+		public BindingList<CartItemModel> Cart
 		{
 			get { return _cart; }
-			set
-			{
+			set {
 				_cart = value;
 				NotifyOfPropertyChange(() => Cart);
 			}
@@ -44,17 +48,28 @@ namespace FedoraDev.TimCo.UserInterface.WPF.ViewModels
 		public int ItemQuantity
 		{
 			get { return _itemQuantity; }
-			set
-			{
+			set {
 				_itemQuantity = value;
 				NotifyOfPropertyChange(() => ItemQuantity);
 				NotifyOfPropertyChange(() => CanAddToCart);
 			}
 		}
 
+		public ProductModel SelectedProduct
+		{
+			get { return _selectedProduct; }
+			set {
+				_selectedProduct = value;
+				NotifyOfPropertyChange(() => SelectedProduct);
+				NotifyOfPropertyChange(() => CanAddToCart);
+			}
+		}
+		#endregion
+
 		public SalesViewModel(IProductEndpoint productEndpoint)
 		{
 			_productEndpoint = productEndpoint;
+			Cart = new BindingList<CartItemModel>();
 		}
 
 		protected override async void OnViewLoaded(object view)
@@ -71,17 +86,60 @@ namespace FedoraDev.TimCo.UserInterface.WPF.ViewModels
 
 		public void AddToCart()
 		{
-			//
+			CartItemModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
+			if (existingItem != null)
+			{
+				existingItem.QuantityInCart += ItemQuantity;
+
+				// HACK - Need to notify of sub-property change within Cart (QuantityInCart)
+				_ = Cart.Remove(existingItem);
+				Cart.Add(existingItem);
+			}
+			else
+			{
+				CartItemModel cartItem = new CartItemModel()
+				{
+					Product = SelectedProduct,
+					QuantityInCart = ItemQuantity
+				};
+				Cart.Add(cartItem);
+			}
+
+			SelectedProduct.QuantityInStock -= ItemQuantity;
+			ItemQuantity = 1;
+			NotifyOfPropertyChange(() => SubTotal);
+			NotifyOfPropertyChange(() => Tax);
+			NotifyOfPropertyChange(() => Total);
 		}
 
 		public void RemoveFromCart()
 		{
-			//
+			NotifyOfPropertyChange(() => SubTotal);
+			NotifyOfPropertyChange(() => Tax);
+			NotifyOfPropertyChange(() => Total);
 		}
 
 		public void Checkout()
 		{
 			//
+		}
+
+		public decimal GetSubTotal()
+		{
+			decimal subTotal = 0;
+			foreach (CartItemModel item in Cart)
+				subTotal += item.Product.RetailPrice * item.QuantityInCart;
+			return subTotal;
+		}
+
+		public decimal GetTax()
+		{
+			return 0;
+		}
+
+		public decimal GetTotal()
+		{
+			return GetSubTotal() + GetTax();
 		}
 	}
 }
