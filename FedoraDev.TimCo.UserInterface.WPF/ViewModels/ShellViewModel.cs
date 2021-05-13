55a5a1a4affe82330fc2bdc10ee1072e284e39a9
FedoraGameDev <fedora.game.dev@gmail.com>
2021-05-13 15:00:18 -0700

    Replace _container.GetInstance reference -> IoC.Get

--- FedoraDev.TimCo.UserInterface.WPF/ViewModels/ShellViewModel.cs
+++ FedoraDev.TimCo.UserInterface.WPF/ViewModels/ShellViewModel.cs
@@ -7,16 +7,14 @@ namespace FedoraDev.TimCo.UserInterface.WPF.ViewModels
 	{
 		private SalesViewModel _salesVM;
 		private IEventAggregator _events;
-		private SimpleContainer _container;
 
-		public ShellViewModel(SalesViewModel salesVM, IEventAggregator events, SimpleContainer container)
+		public ShellViewModel(SalesViewModel salesVM, IEventAggregator events)
 		{
 			_salesVM = salesVM;
 			_events = events;
-			_container = container;
 
 			_events.Subscribe(this);
-			ActivateItem(_container.GetInstance<LoginViewModel>());
+			ActivateItem(IoC.Get<LoginViewModel>());
 		}
 
 		public void Handle(LoginEvent message)
