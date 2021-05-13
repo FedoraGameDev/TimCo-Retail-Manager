90547fb80ff6f9c5c5ac43ae2b4cb1b5fe61b3a7
FedoraGameDev <fedora.game.dev@gmail.com>
2021-05-13 14:57:57 -0700

    Moved Entity Framework Database -> EF-Data

--- FedoraDev.TimCo.DataManager/Models/IdentityModels.cs
+++ FedoraDev.TimCo.DataManager/Models/IdentityModels.cs
@@ -11,8 +11,8 @@ namespace FedoraDev.TimCo.DataManager.Models
     {
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
         {
-            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+			// Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+			ClaimsIdentity userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
             // Add custom user claims here
             return userIdentity;
         }
@@ -20,10 +20,7 @@ namespace FedoraDev.TimCo.DataManager.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
+        public ApplicationDbContext() : base("EF-Data", throwIfV1Schema: false) { }
         
         public static ApplicationDbContext Create()
         {
