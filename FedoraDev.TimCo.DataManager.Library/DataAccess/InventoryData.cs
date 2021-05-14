9eb0561265de4eaf26a24660f9d6165fc854df47
FedoraGameDev <fedora.game.dev@gmail.com>
2021-05-14 01:04:30 -0700

    Added Inventory management data access

---
+++ FedoraDev.TimCo.DataManager.Library/DataAccess/InventoryData.cs
@@ -0,0 +1,25 @@
+﻿using FedoraDev.TimCo.DataManager.Library.Internal.DataAccess;
+using FedoraDev.TimCo.DataManager.Library.Models;
+using System.Collections.Generic;
+
+namespace FedoraDev.TimCo.DataManager.Library.DataAccess
+{
+	public class InventoryData
+	{
+		public List<InventoryModel> GetInventory()
+		{
+			SqlDataAccess sql = new SqlDataAccess();
+
+			var parameters = new { };
+			return sql.LoadData<InventoryModel, dynamic>("spInventoryGetAll", parameters, "TimCo-Data");
+		}
+
+		public void SaveInventoryItem(InventoryModel itemData)
+		{
+			SqlDataAccess sql = new SqlDataAccess();
+
+			sql.SaveData("spInventoryInsert", itemData, "TimCo-Data");
+		}
+	}
+
+}
