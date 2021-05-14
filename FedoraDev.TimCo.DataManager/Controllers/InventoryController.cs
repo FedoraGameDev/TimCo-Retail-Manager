08776f8679e6202f7931bb00ac1fe7e722467253
Jimmy Satterfield <fedora.game.dev@gmail.com>
2021-05-14 01:16:48 -0700

    Added InventoryController with Post and Get routes

---
+++ FedoraDev.TimCo.DataManager/Controllers/InventoryController.cs
@@ -0,0 +1,25 @@
+﻿using FedoraDev.TimCo.DataManager.Library.DataAccess;
+using FedoraDev.TimCo.DataManager.Library.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace FedoraDev.TimCo.DataManager.Controllers
+{
+    [Authorize]
+	public class InventoryController : ApiController
+    {
+        [HttpGet]
+        public List<InventoryModel> Get()
+		{
+            InventoryData inventory = new InventoryData();
+            return inventory.GetInventory();
+		}
+
+        [HttpPost]
+        public void Post(InventoryModel itemData)
+		{
+            InventoryData inventory = new InventoryData();
+            inventory.SaveInventoryItem(itemData);
+		}
+    }
+}
