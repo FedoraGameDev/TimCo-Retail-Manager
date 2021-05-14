0bfe3010156a6ca965a20f2d35c732aa3cfecb9b
FedoraGameDev <fedora.game.dev@gmail.com>
2021-05-14 01:08:19 -0700

    Added Sales Report Route to SaleController

--- FedoraDev.TimCo.DataManager/Controllers/SaleController.cs
+++ FedoraDev.TimCo.DataManager/Controllers/SaleController.cs
@@ -3,6 +3,7 @@ using FedoraDev.TimCo.DataManager.Library.Models;
 using FedoraDev.TimCo.DataManager.Models;
 using Microsoft.AspNet.Identity;
 using System;
+using System.Collections.Generic;
 using System.Web.Http;
 
 namespace FedoraDev.TimCo.DataManager.Controllers
@@ -17,6 +18,14 @@ namespace FedoraDev.TimCo.DataManager.Controllers
             string userId = RequestContext.Principal.Identity.GetUserId();
 
             saleData.SaveSale(sale, userId);
+		}
+
+        [HttpGet, Route("GetSalesReport")]
+        public List<SaleReportModel> GetSalesReport()
+		{
+            SaleData saleData = new SaleData();
+
+            return saleData.GetSaleReport();
 		}
     }
 }
