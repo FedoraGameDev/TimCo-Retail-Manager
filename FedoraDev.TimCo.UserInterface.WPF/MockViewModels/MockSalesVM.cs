46e61733e6539dde18f9377da2a895c2ba90f8fc
Jimmy Satterfield <fedora.game.dev@gmail.com>
2021-05-15 03:23:50 -0700

    Added Mock data

---
+++ FedoraDev.TimCo.UserInterface.WPF/MockViewModels/MockSalesVM.cs
@@ -0,0 +1,38 @@
+﻿using FedoraDev.TimCo.UserInterface.Library.Models;
+using FedoraDev.TimCo.UserInterface.WPF.Models;
+using System.Collections.ObjectModel;
+
+namespace FedoraDev.TimCo.UserInterface.WPF.MockViewModels
+{
+	public class MockSalesVM
+	{
+		public ObservableCollection<ProductWPFModel> Products { get; set; }
+		public ObservableCollection<CartItemWPFModel> Cart { get; set; }
+
+		public MockSalesVM()
+		{
+			ProductWPFModel productMock1 = new ProductWPFModel()
+			{
+				ProductName = "Cool Item Bro",
+				RetailPrice = 15.32m,
+				QuantityInStock = 30
+			};
+
+			ProductWPFModel productMock2 = new ProductWPFModel()
+			{
+				ProductName = "Another Item",
+				RetailPrice = 102.99m,
+				QuantityInStock = 5
+			};
+
+			CartItemWPFModel cartItemMock1 = new CartItemWPFModel()
+			{
+				Product = productMock1,
+				QuantityInCart = 3
+			};
+
+			Products = new ObservableCollection<ProductWPFModel>() { productMock1, productMock2 };
+			Cart = new ObservableCollection<CartItemWPFModel>() { cartItemMock1 };
+		}
+	}
+}
---
+++ FedoraDev.TimCo.UserInterface.WPF/MockViewModels/MockUserDisplayVM.cs
@@ -0,0 +1,31 @@
+﻿using FedoraDev.TimCo.UserInterface.Library.Models;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace FedoraDev.TimCo.UserInterface.WPF.MockViewModels
+{
+	public class MockUserDisplayVM
+	{
+		public ObservableCollection<UserModel> Users { get; set; }
+
+		public MockUserDisplayVM()
+		{
+			UserModel mock1 = new UserModel() {
+				EmailAddress = "some@guy.com",
+				Roles = new Dictionary<string, string>() {
+					{ "0", "Cashier" },
+					{ "1", "Admin" }
+				}
+			};
+
+			UserModel mock2 = new UserModel() {
+				EmailAddress = "whata@man.com",
+				Roles = new Dictionary<string, string>() {
+					{ "2", "Manager" }
+				}
+			};
+
+			Users = new ObservableCollection<UserModel>() { mock1, mock2 };
+		}
+	}
+}
